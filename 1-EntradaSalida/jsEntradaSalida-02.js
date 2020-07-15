b3574a3100e6a92aec252ce29af8694d859b8104
patriciavillalba <patrikiavvv@gmail.com>
2020-07-15 16:14:58 -0300

    un mensaje

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,10 @@
+Bienvenidos.
+Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	alert("Esto funciona de maravilla");
 }
 
